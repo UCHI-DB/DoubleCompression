cad876fc00743c7ef5a0419fa995e9ee59736364
Jacob Spiegel <jsamuelspiegel@gmail.com>
2019-05-14 12:01:33 -0500

    more tests, implemented (D)FCM -> Sprintz pipelining but it isn't working yet

--- src/DoubleCompression/Main.java
+++ src/DoubleCompression/Main.java
@@ -7,33 +7,33 @@ public class Main {
 
 	public static void main(String[] args) throws FileNotFoundException {
 
-//		try {
-//			Tester.generateReport("Gorilla"); //warm up
-//			Tester.generateReport("FCMGorilla");
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
+		try {
+			Tester.generateReport("Gorilla"); //warm up
+			//Tester.generateReport("FCMGorilla");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 //		Tester.testCorrectnessFull("DFCM");
 
-		String name = "Worms_66";
-		try {
-			String method = "FCMGorilla";
-			double[] raw = Reader.readRaw(Util.rawPathify(name));
-			double[] decompressed = Compressor.directDecompress(Compressor.compressFile(name, method),method);
-			int len = raw.length;
-			boolean correct = true;
-			for(int i = 0; i < len; i++) {
-				if(Math.abs(raw[i] - decompressed[i]) > 0.0001) {
-					System.out.println(raw[i] + " != " + decompressed[i]);
-					correct = false;
-				}
-			}
-			System.out.println(correct);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//		String name = "Worms_66";
+//		try {
+//			String method = "FCMGorilla";
+//			double[] raw = Reader.readRaw(Util.rawPathify(name));
+//			double[] decompressed = Compressor.directDecompress(Compressor.compressFile(name, method),method);
+//			int len = raw.length;
+//			boolean correct = true;
+//			for(int i = 0; i < len; i++) {
+//				if(Math.abs(raw[i] - decompressed[i]) > 0.0001) {
+//					System.out.println(raw[i] + " != " + decompressed[i]);
+//					correct = false;
+//				}
+//			}
+//			System.out.println(correct);
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 
 //		double[] data = new double[] {3,4,6,1,2,3,4,5,
 //									  0,0,0,0,0,0,0,0,
